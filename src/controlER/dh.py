09c53d170c6154ad89775d46eaec54a6bad0660c
Joana Simões <joanasimoes04@gmail.com>
2022-10-06 23:25:19 +0100

    Add DH iv exchange

--- src/controlER/dh.py
+++ src/controlER/dh.py
@@ -9,13 +9,25 @@ from cryptography.hazmat.primitives import hashes
 class DH:
     def __init__(self, communication):
         self.communication = communication
-        self.parameters = dh.generate_parameters(generator=2, key_size=2048)
-        self.private_key = self.parameters.generate_private_key()
+        # generate key
+        self.parameters_key = dh.generate_parameters(generator=2, key_size=2048)
+        self.private_key = self.parameters_key.generate_private_key()
         self.public_key = self.private_key.public_key()
-        self.communication.add_value('P', self.parameters.parameter_numbers().p)
-        self.communication.add_value('G', self.parameters.parameter_numbers().g)
+        # write values
+        self.communication.add_value('P_key', self.parameters_key.parameter_numbers().p)
+        self.communication.add_value('G_key', self.parameters_key.parameter_numbers().g)
         self.communication.write_bytes(self.public_key.public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo), 'DHcontrolKey')
         
+        # generate nonce
+        self.parameters_iv = dh.generate_parameters(generator=2, key_size=2048)
+        self.private_iv = self.parameters_iv.generate_private_key()
+        self.public_iv = self.private_iv.public_key()
+        #write values nonce
+        self.communication.add_value('P_iv', self.parameters_iv.parameter_numbers().p)
+        self.communication.add_value('G_iv', self.parameters_iv.parameter_numbers().g)
+        self.communication.write_bytes(self.public_iv.public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo), 'DHcontrolIV')
+
+
     def calculate_private_key(self):
         decoded = self.communication.get_bytes('DHdelenttureKey')
         other_public_key = load_pem_public_key(decoded)
@@ -26,8 +38,25 @@ class DH:
                 algorithm = algorithm, 
                 length=32,
                 salt=None, 
-                info=b'handshake data',
+                info=b'handshake data', # ! o que é suposto ser aqui
                 backend=default_backend()
             ).derive(self.shared_secret)
             return self.common_private_key   
+        return None
+
+    def calculate_private_iv(self):
+        decoded = self.communication.get_bytes('DHDelenttureIV')
+        other_public_key = load_pem_public_key(decoded)
+        algorithm = hashes.SHA256() # TODO: change algorithm
+        if other_public_key:
+            self.shared_secret_iv = self.private_iv.exchange(other_public_key)
+            self.common_private_iv = HKDF(
+                algorithm = algorithm, 
+                length=32,
+                salt=None,
+                info=b'handshake data',
+                backend=default_backend()
+            ).derive(self.shared_secret_iv)
+            return self.common_private_iv
+            
         return None
--- src/delentture/dh.py
+++ src/delentture/dh.py
@@ -9,14 +9,27 @@ from cryptography.hazmat.primitives import hashes
 class DH:
     def __init__(self, communication):
         self.communication = communication
-        p = self.communication.get_value('P')
-        g = self.communication.get_value('G')
-        self.pn = dh.DHParameterNumbers(p, g)
-        self.parameters = self.pn.parameters()
-        self.private_key = self.parameters.generate_private_key()
+        # generate key
+        p_key = self.communication.get_value('P_key')
+        g_key = self.communication.get_value('G_key')
+        self.pn_key = dh.DHParameterNumbers(p_key, g_key)
+        self.parameters_key = self.pn_key.parameters()
+        self.private_key = self.parameters_key.generate_private_key()
         self.public_key = self.private_key.public_key()
+        #write value
         self.communication.write_bytes(self.public_key.public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo), 'DHdelenttureKey')
         
+        #generate nonce
+        p_iv = self.communication.get_value('P_iv')
+        g_iv = self.communication.get_value('G_iv')
+        self.pn_iv = dh.DHParameterNumbers(p_iv, g_iv)
+        self.parameters_iv = self.pn_iv.parameters()
+        self.private_iv = self.parameters_iv.generate_private_key()
+        self.public_iv = self.private_iv.public_key()
+        # write public value
+        self.communication.write_bytes(self.public_iv.public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo), 'DHDelenttureIV')
+
+
     def calculate_private_key(self):
         decoded = self.communication.get_bytes('DHcontrolKey')
         other_public_key = load_pem_public_key(decoded)
@@ -31,5 +44,22 @@ class DH:
                 backend=default_backend()
             ).derive(self.shared_secret)
             return self.common_private_key
+            
+        return None
+
+    def calculate_private_iv(self):
+        decoded = self.communication.get_bytes('DHcontrolIV')
+        other_public_key = load_pem_public_key(decoded)
+        algorithm = hashes.SHA256() # TODO: change algorithm
+        if other_public_key:
+            self.shared_secret_iv = self.private_iv.exchange(other_public_key)
+            self.common_private_iv = HKDF(
+                algorithm = algorithm, 
+                length=32,
+                salt=None,
+                info=b'handshake data',
+                backend=default_backend()
+            ).derive(self.shared_secret_iv)
+            return self.common_private_iv
             
         return None
